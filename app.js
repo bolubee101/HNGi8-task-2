9c4f583848f31cf0e854515e572704f4601c7a9f
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2021-08-18 13:22:40 +0100

    Saving to DB

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
-const express=require("express");
-const cors=require("cors");
-const emailer=require("./emailer").sendMail;
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const emailer = require("./emailer").sendMail;
+const app = express();
+const submission = require("./submissionModel")
+
+mongoose.connect(process.env.DBURI,{ useUnifiedTopology: true, useNewUrlParser: true  }, (err) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log("connected to DB successfully")
+    }
+})
 
 app.use(cors());
 app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded())
 
-let port=process.env.PORT||3001
+let port = process.env.PORT || 3001
 
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+`public/index.html`)
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + `public/index.html`)
 })
 
-app.post("/form-submit",(req,res)=>{
+app.post("/form-submit", async (req, res) => {
     console.log(req.body)
-        emailer(req.body.email)
-    res.status(201).json({
-        status:"success"
-    })
+    let sub = new submission(req.body);
+    try {
+        await sub.save();
+        emailer(req.body.email, "HNG backend task 2", `You filled this out in your form:  "${req.body.subject}"`)
+        res.status(201).json({
+            status: "success"
+        })
+    } catch (err) {
+        return res.status(400).json({
+            status: "error"
+        })
+    }
+
 })
 
 
-app.listen(port,()=>{
-console.log("server started");
+app.listen(port, () => {
+    console.log("server started");
 })
---
+++ submissionModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+let submissionSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+});
+
+let submission = mongoose.model("Submission", submissionSchema);
+module.exports = submission;
